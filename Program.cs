aeb0406443e63fb039cee3cbeaa794677fa7ab4f
[LaleSmdva] <[6vrseyy@code.edu.az]>
2022-11-01 20:21:57 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Array___Object__Big_O_Notation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+            int average,sumNums=0;
+            int[] nums = {10,20,30,40};
+            for(int i = 0; i < nums.Length; i++)
+            {
+                sumNums+=nums[i];
+            }
+            Console.WriteLine("Sum of this array is " +sumNums);
+            average=sumNums/nums.Length;
+            Console.WriteLine("Average is "+average);
+
+
+            #endregion
+
+            #region task2
+               
+            int countPositive=0, countNegative=0;
+            int[] nums = {-5,-4,-3,6,7};
+            for(int i=0;i<nums.Length;i++)
+            {
+                    if(nums[i]==0)
+            {
+                Console.WriteLine("0 is neither positive nor negative");
+            }
+                if(nums[i]>0)
+                {
+                    countPositive++;
+                    
+                }
+                if(nums[i]<0)
+                {
+                    countNegative++;
+                    
+                }
+              
+            }
+            Console.WriteLine("There are "+countPositive+" positive numbers in this array");
+             Console.WriteLine("There are "+countNegative+" negative numbers in this array");
+
+            #endregion
+
+            #region task3
+                 int[] nums = {4,45,85,2,335,33};
+            int maxNum=nums[0];
+            for(int i=0;i<nums.Length;i++)
+            {
+                if(nums[i]>maxNum)
+                {
+                    maxNum=nums[i];
+                    
+                }
+            }
+            Console.WriteLine(maxNum+" is the greatest number");
+            #endregion
+        }
+    }
+}
